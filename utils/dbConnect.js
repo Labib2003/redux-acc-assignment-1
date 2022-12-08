41034036e596b5eb7ca5479c6e72f7d3136e842c
Labib2003 <labib.salimi30@gmail.com>
2022-12-08 07:39:46 +0600

    connected with the database

---
+++ utils/dbConnect.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.ehsradf.mongodb.net/?retryWrites=true&w=majority`;
+
+const dbConnect = async () => {
+  const res = await mongoose
+    .connect(uri)
+    .then(mongoose.connection)
+    .catch((err) => console.log(err));
+  console.log("Database connection successful.");
+};
+
+module.exports = dbConnect;
