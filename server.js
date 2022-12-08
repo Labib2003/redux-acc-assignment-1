3d39e62a63f23775fdd667f151baa0668168a2c1
Labib2003 <labib.salimi30@gmail.com>
2022-12-08 07:41:06 +0600

    connected with the database

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-const dotenv = require("dotenv");
+const dotenv = require("dotenv").config();
 
 const app = require("./index");
+const dbConnect = require("./utils/dbConnect");
 const port = process.env.PORT || 6969;
+
+dbConnect();
 
 app.listen(port, () => {
   console.log(`Server is running at port: ${port}`);
